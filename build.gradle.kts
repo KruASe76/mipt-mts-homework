7e949f9aeea9bf1511bfd9a189e9c04797429b1d
KruASe76 <kruanse@ya.ru>
2024-09-26 11:28:33 +0300

    setup gradle

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    java
+    application
+    id("com.gradleup.shadow") version "8.3.2"
+}
+
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+application {
+    mainClass = "me.kruase.mipt_mts_homework.Main"
+}
+
+
+tasks.shadowJar {
+    archiveClassifier = ""
+}
+
+tasks.distTar {
+    dependsOn(tasks.shadowJar)
+}
+
+tasks.distZip {
+    dependsOn(tasks.shadowJar)
+}
+
+tasks.startShadowScripts {
+    dependsOn(tasks.jar)
+}
+
+tasks.startScripts {
+    dependsOn(tasks.shadowJar)
+}
+
+
+version = "1.2"
