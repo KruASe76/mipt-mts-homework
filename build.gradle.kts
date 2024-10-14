1fc0f1c0e47f553224d9ebe3c4ffc06b61b8852b
KruASe76 <kruanse@ya.ru>
2024-10-14 04:33:45 +0300

    setup JUnit and JaCoCo

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,17 @@ repositories {
 }
 
 plugins {
-    java
     application
+    jacoco
     id("com.gradleup.shadow") version "8.3.2"
 }
 
 
 dependencies {
+    testImplementation(libs.junit.jupiter)
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
     compileOnly("org.jetbrains:annotations:25.0.0")
 }
 
@@ -24,6 +28,10 @@ application {
     mainClass = "me.kruase.mipt.Main"
 }
 
+
+tasks.build {
+    dependsOn(tasks.jacocoTestCoverageVerification)
+}
 
 tasks.shadowJar {
     archiveClassifier = ""
@@ -45,5 +53,29 @@ tasks.startScripts {
     dependsOn(tasks.shadowJar)
 }
 
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
 
-version = "1.2"
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            element = "PACKAGE"
+            excludes = listOf("me.kruase.mipt")
+
+            limit {
+                counter = "LINE"
+                value = "COVEREDRATIO"
+                minimum = BigDecimal.valueOf(0.55)
+            }
+        }
+    }
+}
+
+
+version = "1.4"
